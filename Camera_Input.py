706af629ee92abf432e69b2f7b84c3d9743ed989
Sw4p <usav.swapnil@gmail.com>
2021-04-02 21:58:54 +0100

    Created a new file and class called Marker to contain different marker functions.

--- Camera_Input.py
+++ Camera_Input.py
@@ -3,6 +3,7 @@ import numpy as np
 import pytesseract
 from PyDictionary import PyDictionary
 import TranslatorGUI as GUI
+import Marker
 import re
 
 pi = True
@@ -26,7 +27,7 @@ def main():
         camera.hflip = True
         rawCapture = PiRGBArray(camera, size=(640, 480))
     else:
-        cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
     # Box of interest coordinates/
     x1 = 100
@@ -36,17 +37,19 @@ def main():
 
     # GUI object
     gui = GUI.Translator_GUI()
+    # Marker object
+    marker = Marker.Marker()
 
-    #while (True):
-    for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
+    while (True):
         # Capture image frame
         if pi == True:
-            frame = frame.array
+            camera.capture(rawCapture, format='bgr', use_video_port=True)
+            frame = rawCapture.array
         else:
             ret, frame = cap.read()
         
         # Identify maker position from the image
-        cX, cY = Idenfity_Marker(frame, [0,-10], displayMask=False)
+        cX, cY = marker.colour(frame, [0,-10], displayMask=False)
         if cX != -1:
             # Draw white dot at the center of the marker
             cv2.circle(frame, (cX, cY), 2, (255, 255, 255), -1)
@@ -90,37 +93,6 @@ def main():
         cap.release()
     
     cv2.destroyAllWindows()
-
-def Idenfity_Marker(image, offsets=[0,0], displayMask=False):
-    # Conver BGR to HSV
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    # Detect red colour. Red colour has two range in HSV, 0-30 and 150-180.
-    # here we are detecting only extreme red i.e. 0-10 and 170-180.
-    colourBoundry = ([0, 120, 70], [10, 255, 255]) #HSV
-    mask1 = Identify_Colour(image, colourBoundry)
-    colourBoundry = ([170, 120, 70], [180, 255, 255]) #HSV
-    mask2 = Identify_Colour(image, colourBoundry)
-    # ORing the masks
-    mask1 = mask1 + mask2
-    # Display mask
-    if displayMask != False:
-        cv2.imshow('mask', mask1)
-
-    # Calculate center of the coloured blob
-    M = cv2.moments(mask1)
-    if M["m00"] != 0:
-        cX = int(M["m10"] / M["m00"]) + offsets[0]
-        cY = int(M["m01"] / M["m00"]) + offsets[1]
-    else:
-        cX, cY = -1, -1
-
-    return cX, cY
-
-def Identify_Colour(image, colourBoundry):
-    lower = np.array(colourBoundry[0], dtype="uint8")
-    upper = np.array(colourBoundry[1], dtype="uint8")
-    mask = cv2.inRange(image, lower, upper)
-    return mask
     
 def Detect_Text_Blob(image):
     # Change color image into grayscale image
